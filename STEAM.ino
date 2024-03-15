1760b5e1236592f81e41d036aae2fc0c51ec3567
Shaurya250 <60682049+Shaurya250@users.noreply.github.com>
2024-03-15 11:25:47 +0530

    Update STEAM.ino
    2nd Update.

--- STEAM.ino
+++ STEAM.ino
@@ -14,7 +14,8 @@
 #define STBY 9
 #define DHTPIN A1
 #define DHTTYPE DHT11
-
+#define TRIGGER_PIN 10
+#define ECHO_PIN 12
 const int offsetA = 1;
 const int offsetB = 1;
 
@@ -24,7 +25,7 @@ Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 DHT dht(DHTPIN, DHTTYPE);
-Servo myservo;
+Servo myServo;
 
 
 void setup() {
@@ -32,15 +33,49 @@ void setup() {
   Serial.begin(9600);
   dht.begin();
   myServo.attach(9);
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for(int i=0, i<=180, i++){
-    myServo.write(i)
-    delay(50)
-    
+  for(int i=15;i<=165;i++){  
+    myServo.write(i);
+    delay(30);
+    int distance = sonar.ping_cm();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+  
+    Serial.print(i); // Sends the current degree into the Serial Port
+    Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+    Serial.print(distance); // Sends the distance value into the Serial Port
+    Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   }
-
+  // Repeats the previous lines from 165 to 15 degrees
+    for(int i=165;i>15;i--){  
+      myServo.write(i);
+      delay(30);
+      int distance = sonar.ping_cm();
+      Serial.print(i);
+      Serial.print(",");
+      Serial.print(distance);
+      Serial.print(".");
+  }
+    String sulfate = Serial.readString();
+    int distance = sonar.ping_cm();
+    if (distance<=15){
+      brake(motor1, motor2);
+      back(motor1, motor2, -150);
+      motor1.drive(255, 1000); 
+      }
+    else if (sulfate=="R"){
+      motor1.drive(255,1000);
+      delay(1000);
+      brake(motor1, motor2);
+    }
+    else if (sulfate=="L"){
+      motor2.drive(255,1000);
+      delay(1000);
+      brake(motor1, motor2);
+    }
+    else if (sulfate=="F"){
+      forward(motor1, motor2, 150);
+      delay(1000);
+      brake(motor1, motor2);
+    }
 }
